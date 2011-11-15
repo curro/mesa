b78b14f51a36c28f86a0a1f3c550b2865833a8f6
Francisco Jerez <currojerez@riseup.net>
2011-11-15 02:06:20 +0100

    nv50/ir: Add support code for calculating the clobber set of a basic block or function.

--- src/gallium/drivers/nv50/codegen/nv50_ir.h
+++ src/gallium/drivers/nv50/codegen/nv50_ir.h
@@ -877,6 +877,7 @@ public:
    Graph::Node dom;
 
    BitSet liveSet;
+   BitSet defSet;
 
    uint32_t binPos;
    uint32_t binSize;
@@ -927,6 +928,7 @@ public:
 
    inline LValue *getLValue(int id);
 
+   void buildDefSets();
    bool convertToSSA();
 
 public:
@@ -950,6 +952,7 @@ public:
 
 private:
    void buildLiveSetsPreSSA(BasicBlock *, const int sequence);
+   void buildDefSetsPreSSA(BasicBlock *bb, const int seq);
 
 private:
    int id;
