49d35614e505b3fee9c740195efa16ce53fa47db
Francisco Jerez <currojerez@riseup.net>
2011-11-15 17:25:40 +0100

    nv50/ir: Clean up before calculating the instruction ordering for a new function.

--- src/gallium/drivers/nv50/codegen/nv50_ir_util.h
+++ src/gallium/drivers/nv50/codegen/nv50_ir_util.h
@@ -332,6 +332,13 @@ public:
       data = (Item *)REALLOC(data, oldSize, size * sizeof(Item));
    }
 
+   void clear()
+   {
+      FREE(data);
+      data = NULL;
+      size = 0;
+   }
+
 private:
    Item *data;
    unsigned int size;
@@ -386,6 +393,12 @@ public:
    };
 
    Iterator iterator() const { return Iterator(this); }
+
+   void clear() {
+      data.clear();
+      ids.clear(true);
+      size = 0;
+   }
 
 private:
    DynArray data;
