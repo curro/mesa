e5adbf3a7f5f0987b84711b35a0da9e6554ef240
Francisco Jerez <currojerez@riseup.net>
2011-11-15 01:29:31 +0100

    nv50/ir/opt: Fix for function calls.

--- src/gallium/drivers/nv50/codegen/nv50_ir_peephole.cpp
+++ src/gallium/drivers/nv50/codegen/nv50_ir_peephole.cpp
@@ -247,7 +247,7 @@ ConstantFolding::visit(BasicBlock *bb)
 
    for (i = bb->getEntry(); i; i = next) {
       next = i->next;
-      if (i->op == OP_MOV) // continue early, MOV appears frequently
+      if (i->op == OP_MOV || i->op == OP_CALL)
          continue;
 
       if (i->srcExists(1) && i->src(0).getImmediate(src0) &&
@@ -2001,7 +2001,8 @@ GlobalCSE::visit(BasicBlock *bb)
       for (s = 1; phi->srcExists(s); ++s) {
          if (phi->getSrc(s)->refCount() > 1)
             break;
-         if (!phi->getSrc(s)->getInsn()->isResultEqual(ik))
+         if (!phi->getSrc(s)->getInsn() ||
+             !phi->getSrc(s)->getInsn()->isResultEqual(ik))
             break;
       }
       if (!phi->srcExists(s)) {
